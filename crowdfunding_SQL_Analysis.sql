85ec0a7c9cd5344aa2aae7fe5b1639ca6f8a4cea
MSF2141 <114106019+MSF2141@users.noreply.github.com>
2023-01-06 11:12:35 -0500

    Update crowdfunding_SQL_Analysis.sql

--- crowdfunding_SQL_Analysis.sql
+++ crowdfunding_SQL_Analysis.sql
@@ -1,5 +1,4 @@
--- Challenge Bonus queries.
--- 1. (2.5 pts)
+-- 1. 
 -- Retrieve all the number of backer_counts in descending order for each `cf_id` for the "live" campaigns. 
 SELECT campaign.cf_id, campaign.outcome, campaign.backers_count 
 INTO backers_count
@@ -9,12 +8,12 @@ GROUP BY campaign.cf_id
 ORDER BY campaign.backers_count DESC;
 
 
--- 2. (2.5 pts)
+-- 2. 
 -- Using the "backers" table confirm the results in the first query.
 SELECT * FROM backers_count; 
 
 
--- 3. (5 pts)
+-- 3. 
 -- Create a table that has the first and last name, and email address of each contact.
 -- and the amount left to reach the goal for all "live" projects in descending order. 
 SELECT campaign.cf_id,
@@ -53,7 +52,7 @@ SELECT * FROM  email_contacts_remaining_goal_amount;
 
 
 
--- 4. (5 pts)
+-- 4. 
 -- Create a table, "email_backers_remaining_goal_amount" that contains the email address of each backer in descending order, 
 -- and has the first and last name of each backer, the cf_id, company name, description, 
 -- end date of the campaign, and the remaining amount of the campaign goal as "Left of Goal". 
